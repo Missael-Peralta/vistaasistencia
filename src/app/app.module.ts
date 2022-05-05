9b247cadfae3b1d0034cd693f8ce7cbf2e4a57e9
Missael-Peralta <rperalta0@ucol.mx>
2022-05-05 12:53:52 -0500

    vita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { HeadComponent } from './components/head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule
